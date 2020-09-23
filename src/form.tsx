e2e2e2a6466e9c3b568174927c407d7debba859c
victimlisten <victimlisten@gmail.com>
2020-09-23 19:20:50 +0700

    test final form with TextInput

--- src/form.tsx
+++ src/form.tsx
@@ -1,21 +1,51 @@
-import {View, Text} from 'react-native';
-import React from 'react';
-import {Field, reduxForm} from 'redux-form';
-import {BaseInput} from './input';
+import {View, Text, Button, TextInput} from 'react-native';
+import React, {useCallback} from 'react';
+import {Field, Form} from 'react-final-form';
+import {FormApi, SubmissionErrors} from 'final-form';
 
-const FormView = () => {
+interface IFormProps {
+  onSubmit: (
+    values: any,
+    form: FormApi<any, any>,
+    callback?: (errors?: SubmissionErrors) => void,
+  ) =>
+    | SubmissionErrors
+    | Promise<SubmissionErrors | undefined>
+    | undefined
+    | void;
+}
+
+const FormWrapper = ({onSubmit}: IFormProps) => {
   return (
-    <View>
-      <Text>form view</Text>
-      <Field component={BaseInput} name={'test'} />
-    </View>
+    <Form
+      onSubmit={onSubmit}
+      render={({handleSubmit}) => (
+        <View>
+          <Field name="FieldName" placeholder="your placeholder">
+            {({input, meta}) => {
+              return (
+                <View>
+                  <TextInput
+                    value={input.value}
+                    onChangeText={input.onChange}
+                  />
+                  <Text>{meta.error}</Text>
+                </View>
+              );
+            }}
+          </Field>
+          <Button color="info" title={'Submit'} onPress={handleSubmit} />
+        </View>
+      )}
+    />
   );
 };
 
-const Form = reduxForm<any, any>({
-  form: 'testForm',
-})(FormView);
-
 export const TestForm = () => {
-  return React.createElement(Form, {});
+  const onSubmit = useCallback((values, form) => {
+    console.log(values);
+    console.log(form);
+    console.log(values);
+  }, []);
+  return <FormWrapper onSubmit={onSubmit} />;
 };
