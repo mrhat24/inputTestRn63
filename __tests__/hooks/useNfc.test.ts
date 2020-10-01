0a7f5edbbb3f47a5c9b541a136bea0d2b591929f
victimlisten <victimlisten@gmail.com>
2020-10-01 11:34:30 +0700

    nfc write test with mocks

--- __tests__/hooks/useNfc.test.ts
+++ __tests__/hooks/useNfc.test.ts
@@ -1,11 +1,11 @@
 import {renderHook, act} from '@testing-library/react-hooks';
 import {useNfc} from '../../src/shared/hooks/useNfc';
 
-test('should increment counter', async () => {
+test('should write nfc tag', async () => {
   const {result} = renderHook(() => useNfc());
 
   await act(async () => {
-    await result.current.writeTag('ass');
+    await result.current.writeTag('test tag');
   });
 
   expect('foo').toBe('foo');
